ff56e21d5bd515b44e39b18fa05fa28bee76f6a6
marco villanueva <villanuevamarco98@gmail.com>
2020-07-23 20:30:36 -0600

    Add project files.

---
+++ LearningNullCond/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LearningNullCond
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Person person= null; //new Person()  { Name = "Tim" };//what if this was null
+            //manual null check 
+            if(person != null)
+            Console.WriteLine(person?.Name);
+            //up next
+        }
+    }
+}
