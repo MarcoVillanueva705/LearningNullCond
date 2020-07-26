9cc399e6258bfe6b2277ddc2ed04b92b80cd11c9
marco villanueva <villanuevamarco98@gmail.com>
2020-07-26 17:18:29 -0600

    default value after  operators

--- LearningNullCond/Program.cs
+++ LearningNullCond/Program.cs
@@ -11,13 +11,26 @@ namespace LearningNullCond
 
         static void Main(string[] args)
         {
+            //When you need to deal with Nulls inside of properties
+            //Great way to clean up code
 
             //Person person= null; new Person()  { Name = "Tim" };//what if this was null
-            Person person = new Person()  { Name = "Tim" };//what if this was null
+            //null reference exception if not instantiated
+            Person person = new Person();  //{ Name = "Tim" };//what if this was null
 
             //manual null check 
             if (person != null)
-            Console.WriteLine(person?.Name.Length);
+            //Console.WriteLine(person?.Name.Length);
+            
+            //if not instantiated, use null conditional operator '?' on name to rid of it
+            //Console.WriteLine(person?.Name?.Length);
+
+            //null coalescing with null conditional operators
+            //we can set a default value to 'not set'
+            Console.WriteLine(person?.Name?? "Not Set");
+            //instead of empty line, you'll see 'not set'
+
+
             //up next, finish code for tomorrow
         }
     }
